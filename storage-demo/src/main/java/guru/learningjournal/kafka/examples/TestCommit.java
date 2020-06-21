bbc59b7af1119ab8c445706c524017711af7c321
hemanth <hemanthforever0610@gmail.com>
2020-06-21 15:39:58 -0500

    hello

---
+++ storage-demo/src/main/java/guru/learningjournal/kafka/examples/TestCommit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package guru.learningjournal.kafka.examples;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TestCommit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
